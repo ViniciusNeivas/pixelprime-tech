47968c12f9a0e53d42f8deef1c04b532ec0ff1a4
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-06-19 16:25:17 -0300

    feat (pagina-nao-encontrada): Criação da página não encontrada em sua primeira versão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
