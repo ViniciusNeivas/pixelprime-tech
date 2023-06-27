825f4127064ce2fa7807b429c2cf96e00a9b22d9
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-06-27 15:33:21 -0300

    Fix (Header): Adição das rotas aos links das páginas de produtos e carrinho

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -10,18 +10,24 @@ itens: IProdutoCarrinho[] = []
   constructor() { }
 
   obtemCarrinho(){
-    const carrinho = JSON.parse(localStorage.getItem("carrinho") || "");
-    return carrinho;
+    this.itens= JSON.parse(localStorage.getItem("carrinho") || "");
+    return this.itens;
   }
 
   adicionarAoCarrinho(produto: IProdutoCarrinho){
     this.itens.push(produto);
     localStorage.setItem("carrinho", JSON.stringify(this.itens))
   }
-
+  
   limparCarrinho(){
     this.itens = [];
     localStorage.clear();
+  }
+  
+  removerProdutoCarrinho(produtoId: number){
+    this.itens = this.itens.filter(item => item.id !== produtoId);
+    localStorage.setItem("carrinho", JSON.stringify(this.itens))
 
+    
   }
 }
